a6cac83c879a1737f808b50ef33a6102aa392c6d
adlmacorra <138655684+adlmacorra@users.noreply.github.com>
2023-10-19 10:11:09 +0200

    Update predict .py

--- predict .py
+++ predict .py
@@ -128,7 +128,24 @@ def fiability(distrito):
         return "Fiabilidad Baja"
 
 def modified_price(predicted_price, area, distrito):
-    #funcion de fiabilidad para tres clases, alta media baja, con la tabla de idealista por barrio
+    """
+    
+    Ponderación de la predicción del modelo según la precisión contra la tabla de estimaciones por m2 de idealista
+    
+    Parametros:
+    - predicted_price: number
+        La predicción de precio del XGB
+    - area: texto
+        Area en metros cuadrados de la vivienda
+    - distrito: texto
+        El input de distrito de Madrid donde esta ubicada la vivienda
+
+    Returns:
+    - modified_price: number
+        Precio tras ponderar según fiabilidad contra la tabla de estimaciones de idealista
+        
+    """
+    
     precision_diana = (.84, .76, .74)
     if fiability(distrito) ==  "Fiabilidad Alta":
         alpha = precision_diana[0]
